d58fd749d0c3a4a84203bb05565936ec2d667b3d
Irving <imzo15@hotmail.com>
2021-01-24 23:23:10 -0700

    Initial server

---
+++ index.js
@@ -0,0 +1,80 @@
+const config = require("./config");
+const http = require("http");
+const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
+const server = http.createServer(function(req, res) {
+    var parsedUrl = url.parse(req.url, true);
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+    var queryStringObject = parsedUrl.query;
+    var method = req.method.toLowerCase();
+    var headers = req.headers;
+
+    // Get payload if there is any
+    var decoder = new StringDecoder("utf-8");
+    var buffer = "";
+    req.on("data", function(data) {
+        buffer += decoder.write(data);
+    });
+
+    req.on("end", function() {
+        buffer += decoder.end();
+        // Choose the handler this request should go to
+        var chosenHandler = typeof(router[trimmedPath]) !== "undefined" ? router[trimmedPath] : handlers.notFound;
+
+        // Construct data object to send to the handler
+        var data = {
+            "trimmedPath": trimmedPath,
+            "queryStringObject": queryStringObject,
+            "method": method,
+            "headers": headers,
+            "payload": buffer,
+        };
+
+        // Route the request to the handler specified in the router
+
+        chosenHandler(data, function(statusCode, payload) {
+            // Use the status code called back by the handler, or default to 200 OK
+            statusCode = typeof(statusCode) == "number" ? statusCode : 200;
+            // Use the payload called back by the handler, or default to an empty ''
+            payload = typeof(payload) == "object" ? payload : {};
+
+            // Convert the payload to a string
+            var payloadString = JSON.stringify(payload);
+
+            res.setHeader("Content-Type", "application/json");
+            res.writeHead(statusCode);
+            // Return the response
+            res.end(payloadString);
+
+
+            console.log(`Request received on path: ${trimmedPath} with method: ${method} and with theser query string parameters `);
+            console.log(queryStringObject);
+            console.log(`Request received with these headers`);
+            console.log(headers);
+            console.log(`Request received with this payload`);
+            console.log(buffer);
+            console.log(`Returning this response: ${statusCode} : ${payloadString}`);
+        });
+
+    });
+});
+
+server.listen(config.port, function() {
+    console.log(`Server listening on port ${config.port} in ${config.envName} mode`);
+});
+
+var handlers = {};
+
+handlers.sample = function(data, callback) {
+    callback(406, { "name": "Sample handler" });
+};
+
+handlers.notFound = function(data, callback) {
+    callback(404);
+};
+
+var router = {
+    'sample': handlers.sample
+};
